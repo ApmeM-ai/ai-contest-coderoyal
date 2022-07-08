c0592b973e31da4aa611c00d843654e0555a4f97
Artem.Votincev <artem.votincev@gmail.com>
2022-07-08 14:37:51 +0300

    Add start bot from examples

--- src/client-csharp/MyStrategy.cs
+++ src/client-csharp/MyStrategy.cs
@@ -1,13 +1,38 @@
+using System.Collections.Generic;
+using AiCup22.Model;
+
 namespace AiCup22
 {
     public class MyStrategy
     {
-        public MyStrategy(AiCup22.Model.Constants constants) {}
-        public AiCup22.Model.Order GetOrder(AiCup22.Model.Game game, DebugInterface debugInterface)
-        {
-            return new AiCup22.Model.Order(new System.Collections.Generic.Dictionary<int, AiCup22.Model.UnitOrder>());
+        private readonly Constants constants;
+
+        public MyStrategy(Constants constants) {
+            this.constants = constants;
         }
-        public void DebugUpdate(DebugInterface debugInterface) {}
-        public void Finish() {}
+
+        public Order GetOrder(Game game, DebugInterface debugInterface)
+        {
+            var orders = new Dictionary<int, UnitOrder>();
+            foreach (var unit in game.Units)
+            {
+                if (unit.PlayerId != game.MyId)
+                {
+                    continue;
+                }
+                orders[unit.Id] = new UnitOrder(
+                    new Vec2(-unit.Position.X, -unit.Position.Y),
+                    new Vec2(-unit.Direction.Y, unit.Direction.X),
+                    new ActionOrder.Aim(true));
+            }
+
+            return new Order(orders);
+        }
+ 
+        public void DebugUpdate(DebugInterface debugInterface) { 
+        }
+ 
+        public void Finish() { 
+        }
     }
 }
