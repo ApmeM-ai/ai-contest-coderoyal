50d7dfff7f6b31c71a9725bf6a9f54ea408febff
Artem.Votincev <artem.votincev@gmail.com>
2022-07-14 20:45:51 +0300

    restructure python bot

--- src/client-python/my_strategy.py
+++ src/client-python/my_strategy.py
@@ -1,6 +1,6 @@
 from model.game import Game
 from debugging import Color
-from model.item import ShieldPotions, Weapon
+from model.item import Ammo, ShieldPotions, Weapon
 from model.order import Order
 from model.unit_order import UnitOrder
 from model.constants import Constants
@@ -16,71 +16,122 @@ class MyStrategy:
         pass
     def get_order(self, game: Game, debug_interface: Optional[DebugInterface]) -> Order:
         orders = {}
-        r = game.zone.next_radius
-        center = game.zone.next_center
-        Enemy = False
-        for unit in game.units:	
-            if unit.player_id == game.my_id:
-                UnitMe = unit
-        for unit in game.units:	    
+        debug_interface.add_placed_text(self.findMe(game).position, str(True), Vec2(0,0), 1, Color(1,0,1,1))   
+        me = self.findMe(game)
+        for unit in game.units:
             if unit.player_id != game.my_id:
-                if Enemy and (((UnitMe.position.x - UnitEnemy.position.x)**2+(UnitMe.position.y - UnitEnemy.position.y)**2)**0.5 < ((UnitMe.position.x - unit.position.x)**2+(UnitMe.position.y - unit.position.y)**2)**0.5):
-                    continue    
-                Enemy = True
-                UnitEnemy = unit
-        hasLoot = False
-        for loot in game.loot:
-            if hasLoot and (((UnitMe.position.x - NearLoot.position.x)**2+(UnitMe.position.y - NearLoot.position.y)**2)**0.5 < ((UnitMe.position.x - loot.position.x)**2+(UnitMe.position.y - loot.position.y)**2)**0.5):
                 continue
-            if not isinstance(loot.item, ShieldPotions):
-                continue
-            if ((center.x - loot.position.x)**2+(center.y - loot.position.y)**2)**0.5 >= r:
-                continue
-            NearLoot = loot
-            hasLoot = True
-        if Enemy:
-            actionMe = ActionOrder.Aim(True)
-            if UnitMe.health < self.const.unit_health:
-                if hasLoot:
-                    UnitPosNext = Vec2(NearLoot.position.x - UnitMe.position.x, NearLoot.position.y - UnitMe.position.y)
-                    actionMe = ActionOrder.Pickup(NearLoot.id)
-                else:
-                    UnitPosNext = Vec2(UnitMe.position.x - UnitEnemy.position.x, UnitMe.position.y - UnitEnemy.position.y)
-            else:
-                UnitPosNext = Vec2(UnitEnemy.position.x - UnitMe.position.x, UnitEnemy.position.y - UnitMe.position.y)
-            UnitDir = Vec2(UnitEnemy.position.x - UnitMe.position.x, UnitEnemy.position.y - UnitMe.position.y)
-        else:
-            if hasLoot:
-                UnitPosNext = Vec2(NearLoot.position.x - UnitMe.position.x, NearLoot.position.y - UnitMe.position.y)
-                UnitDir = Vec2(NearLoot.position.x - UnitMe.position.x, NearLoot.position.y - UnitMe.position.y)
-                actionMe = ActionOrder.Pickup(NearLoot.id)
-            else:
-                actionMe = ActionOrder.Aim(False)
-                if UnitMe.health < self.const.unit_health:
-                    havesound = False
-                    for unit in game.sounds:
-                        if havesound and (((UnitMe.position.x - MakeSound.position.x)**2+(UnitMe.position.y - MakeSound.position.y)**2)**0.5 < ((UnitMe.position.x - unit.position.x)**2+(UnitMe.position.y - unit.position.y)**2)**0.5):
-                            continue
-                        havesound = True
-                        MakeSound = unit
-                    if havesound:
-                        UnitPosNext = Vec2(MakeSound.position.x - UnitMe.position.x, MakeSound.position.y - UnitMe.position.y)
-                        UnitDir = Vec2(MakeSound.position.x - UnitMe.position.x, MakeSound.position.y - UnitMe.position.y)
-                    else:
-                        UnitPosNext = Vec2(center.x-UnitMe.position.x, center.y-UnitMe.position.y)
-                        UnitDir = Vec2(-UnitMe.direction.y, UnitMe.direction.x) 
-                    debug_interface.add_placed_text(UnitMe.position, str(True), Vec2(0,0), 1, Color(1,0,1,1))
-                else:
-                    UnitPosNext = Vec2(center.x-UnitMe.position.x, center.y-UnitMe.position.y)
-                    UnitDir = Vec2(-UnitMe.direction.y, UnitMe.direction.x)            
-        if UnitMe.health < self.const.unit_health and UnitMe.shield_potions:
-            actionMe = ActionOrder.UseShieldPotion()
-        orders[UnitMe.id] = UnitOrder(
-            UnitPosNext,
-            UnitDir,
-            actionMe)
+            orders[unit.id] = UnitOrder(
+                self.nextPoint(game, me),
+                self.nextDir(game,me),
+                self.nextAction(game,me))      
         return Order(orders)
     def debug_update(self, displayed_tick: int, debug_interface: DebugInterface):
         pass
     def finish(self):
         pass
+    def distanceM(self, point1, point2):
+        dist = ((point1.position.x - point2.position.x)**2+(point1.position.y - point2.position.y)**2)**0.5
+        return dist
+    def hasEnemy(self, myUnit, enemyUnit):
+        if myUnit.health < self.const.unit_health:
+            UnitPosNext = Vec2(myUnit.position.x - myUnit.position.x, myUnit.position.y - myUnit.position.y)
+        else:
+            UnitPosNext = Vec2(enemyUnit.position.x - myUnit.position.x, enemyUnit.position.y - myUnit.position.y)
+        UnitDir = Vec2(enemyUnit.position.x - myUnit.position.x, enemyUnit.position.y - myUnit.position.y)
+        return UnitOrder(
+            UnitPosNext,
+            UnitDir,
+            ActionOrder.Aim(True))
+    def nextPoint(self, game, myUnit):
+        nearestEnemy = self.findNearestEnemy(game,myUnit)
+        if myUnit.weapon == 0 or myUnit.weapon == 1 or myUnit.wepon == None:
+            point = self.findLoot(game, myUnit, Weapon)
+        if nearestEnemy:
+            if myUnit.health < self.const.unit_health:
+                point = Vec2(myUnit.position.x - nearestEnemy.position.x, myUnit.position.y - nearestEnemy.position.y)
+            else:
+                point = Vec2(nearestEnemy.position.x - myUnit.position.x, nearestEnemy.position.y - myUnit.position.y)
+        else:
+            nearestLoot = self.findLoot(game, myUnit, ShieldPotions)
+            if nearestLoot:
+                point = Vec2(nearestLoot.position.x - myUnit.position.x, nearestLoot.position.y - myUnit.position.y)
+            else:
+                if myUnit.weapon == 0 or myUnit.weapon == 1:
+                    nearestLoot = self.findLoot(game, myUnit, Weapon)
+                    if nearestLoot:
+                        point = Vec2(nearestLoot.position.x - myUnit.position.x, nearestLoot.position.y - myUnit.position.y)
+                    else:
+                        point = game.zone.next_center
+                else:
+                    nearestLoot = self.findLoot(game, myUnit, Ammo)
+                    if nearestLoot:
+                        point = Vec2(nearestLoot.position.x - myUnit.position.x, nearestLoot.position.y - myUnit.position.y)
+                    else:
+                        point = game.zone.next_center
+        return point
+    def nextDir(self, game,myUnit):
+        nearestEnemy = self.findNearestEnemy(game,myUnit)
+        if nearestEnemy:
+            direction = Vec2(nearestEnemy.position.x - myUnit.position.x, nearestEnemy.position.y - myUnit.position.y)
+        else:
+            direction = Vec2(-myUnit.direction.y, myUnit.direction.x)
+        return direction
+    def nextAction(self, game,myUnit):
+        nearestEnemy = self.findNearestEnemy(game,myUnit)
+        if nearestEnemy:
+            action = ActionOrder.Aim(True)
+        else:
+            action = ActionOrder.Aim(False)
+        nearestLoot = self.findLoot(game,myUnit,ShieldPotions)
+        if nearestLoot and self.distanceM(myUnit,nearestLoot) < self.const.unit_radius:
+            action = ActionOrder.Pickup(nearestLoot.id)
+        nearestLoot = self.findLoot(game,myUnit,Weapon)
+        if nearestLoot and self.distanceM(myUnit,nearestLoot) < self.const.unit_radius:
+            action = ActionOrder.Pickup(nearestLoot.id)
+        nearestLoot = self.findLoot(game,myUnit,Ammo)
+        if nearestLoot and self.distanceM(myUnit,nearestLoot) < self.const.unit_radius:
+            action = ActionOrder.Pickup(nearestLoot.id)
+        if myUnit.shield < self.const.max_shield and myUnit.shield_potions:
+            action = ActionOrder.UseShieldPotion()
+        return action
+    def findLoot(self, game, myUnit, classLoot):
+        r = game.zone.next_radius
+        center = game.zone.next_center
+        hasLoot = False
+        for loot in game.loot:
+            if hasLoot and self.distanceM(myUnit,NearLoot) < self.distanceM(myUnit,loot):
+                continue
+            if not isinstance(loot.item, classLoot):
+                continue
+            if ((center.x - loot.position.x)**2+(center.y - loot.position.y)**2)**0.5 >= r:
+                continue
+            if classLoot == Weapon:
+                if loot.item.type_index == 0 or loot.item.type_index == 1:
+                    continue
+            if classLoot == Ammo:
+                if loot.item.weapon_type_index == myUnit.weapon:
+                    continue
+            NearLoot = loot
+            hasLoot = True
+        if hasLoot:
+            return NearLoot
+        else:
+            return False
+    def findMe(self, game):
+        for unit in game.units:	
+            if unit.player_id == game.my_id:
+                me = unit
+        return me
+    def findNearestEnemy(self, game, myUnit):
+        Enemy = False
+        for unit in game.units:	    
+            if unit.player_id != game.my_id:
+                if Enemy and self.distanceM(myUnit, nearestEnemy) < self.distanceM(myUnit, unit):
+                    continue    
+                Enemy = True
+                nearestEnemy = unit
+        if Enemy:
+            return nearestEnemy
+        else:
+            return False
